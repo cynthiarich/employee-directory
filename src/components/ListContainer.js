86a6ecc618120b207873649a3de9176b746eb524
Cynthia Rich <crichID@github.com>
2020-03-25 02:29:06 -0400

    small style tweaks

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -9,37 +9,32 @@ const ListContainer = () => {
     return (
         <div className="row mt-0">
             <div className="col-12 mb-2">
-                <div className="navbar navbar-expand-lg mavbar-light bg-light" >
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#listNavContent" aria-controls="listNavContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="listNavContent">
-                        <form className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-2" type="search" onChange={(event) => searches.onChange("search", event.target.value)} placeholder="Search" aria-label="Search" value={searches.search}/>
-                            <button className="btn btn-outline-secondary my-2 my-sm-0 mr-4" type="submit" onClick={(event) => {
-                                event.preventDefault();
-                                searches.onClick(event.target.value)
-                            }}>Search</button>
-                            <div className="form-check">
-                                <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("all", "")} name="filterRadios" id="allRadio" value="all" checked={searches.type === "all"} />
-                                <label className="form-check-label mr-2" htmlFor="allRadio">
-                                    All
+                <div className="navbar navbar-expand-lg navbar-light bg-light" >
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" onChange={(event) => searches.onChange("search", event.target.value)} placeholder="Search" aria-label="Search" value={searches.search} />
+                        <button className="btn btn-outline-secondary my-2 my-sm-0 mr-4" type="submit" onClick={(event) => {
+                            event.preventDefault();
+                            searches.onClick(event.target.value)
+                        }}>Search</button>
+                        <div className="form-check">
+                            <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("all", "")} name="filterRadios" id="allRadio" value="all" checked={searches.type === "all"} />
+                            <label className="form-check-label mr-2" htmlFor="allRadio">
+                                All
                                 </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("birthday", "")} name="filterRadios" id="bdayRadio" value="birthday" checked={searches.type === "birthday"} />
-                                <label className="form-check-label mr-2" htmlFor="bdayRadio">
-                                    Birthdays
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("birthday", "")} name="filterRadios" id="bdayRadio" value="birthday" checked={searches.type === "birthday"} />
+                            <label className="form-check-label mr-2" htmlFor="bdayRadio">
+                                Birthdays
                                 </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("anniversary", "")} name="filterRadios" id="annivRadio" value="anniversary" checked={searches.type === "anniversary"} />
-                                <label className="form-check-label mr-2" htmlFor="annivRadio">
-                                    Anniversaries
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input mr-2" type="radio" onChange={() => searches.onChange("anniversary", "")} name="filterRadios" id="annivRadio" value="anniversary" checked={searches.type === "anniversary"} />
+                            <label className="form-check-label mr-2" htmlFor="annivRadio">
+                                Anniversaries
                                 </label>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
             <EmployeeList />
